95965df2326426dfdc13d35a02ec4f64a12f6f56
Justus Magin <keewis@posteo.de>
2022-11-08 11:12:06 +0100

    fix the name of the parameter

--- test_example.py
+++ test_example.py
@@ -35,6 +35,6 @@ def test_error(failing_fixture):
     pass
 
 
-@pytest.mark.parametrize("failure", range(200))
+@pytest.mark.parametrize("param", range(200))
 def test_parametrized_failure(param):
     raise ValueError("invalid value: {param}. Try something else.")
